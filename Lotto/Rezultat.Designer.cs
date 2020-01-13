5330e1a900dd37086bc39c52a3150c6b9e17c084
Matej Lipovača <clicker34@gmail.com>
2020-01-13 15:40:47 +0100

    Final
    Final

--- Lotto/Rezultat.Designer.cs
+++ Lotto/Rezultat.Designer.cs
@@ -38,7 +38,14 @@
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.btnPonovo = new System.Windows.Forms.Button();
+            this.btnZatvori = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // naslovLbl
@@ -86,7 +93,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label2.ForeColor = System.Drawing.Color.DimGray;
-            this.label2.Location = new System.Drawing.Point(346, 413);
+            this.label2.Location = new System.Drawing.Point(330, 413);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(262, 46);
             this.label2.TabIndex = 4;
@@ -96,7 +103,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label4.Location = new System.Drawing.Point(174, 322);
+            this.label4.Location = new System.Drawing.Point(186, 205);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(0, 39);
             this.label4.TabIndex = 6;
@@ -122,8 +129,9 @@
             // label7
             // 
             this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label7.Location = new System.Drawing.Point(174, 209);
+            this.label7.Location = new System.Drawing.Point(186, 331);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(0, 39);
             this.label7.TabIndex = 9;
@@ -134,29 +142,122 @@
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label8.ForeColor = System.Drawing.Color.DarkRed;
-            this.label8.Location = new System.Drawing.Point(123, 499);
+            this.label8.Location = new System.Drawing.Point(157, 499);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(179, 37);
             this.label8.TabIndex = 10;
             this.label8.Text = "Osvojili ste ";
             // 
-            // button1
+            // label9
             // 
-            this.button1.Location = new System.Drawing.Point(295, 547);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 11;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.Location = new System.Drawing.Point(238, 331);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 39);
+            this.label9.TabIndex = 11;
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label10.Location = new System.Drawing.Point(341, 331);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 39);
+            this.label10.TabIndex = 13;
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.Color.Transparent;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label11.Location = new System.Drawing.Point(290, 331);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 39);
+            this.label11.TabIndex = 12;
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.Color.Transparent;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label12.Location = new System.Drawing.Point(493, 331);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(0, 39);
+            this.label12.TabIndex = 16;
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.BackColor = System.Drawing.Color.Transparent;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label13.Location = new System.Drawing.Point(442, 331);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(0, 39);
+            this.label13.TabIndex = 15;
+            this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.BackColor = System.Drawing.Color.Transparent;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label14.Location = new System.Drawing.Point(391, 331);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(0, 39);
+            this.label14.TabIndex = 14;
+            this.label14.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnPonovo
+            // 
+            this.btnPonovo.BackColor = System.Drawing.Color.DarkRed;
+            this.btnPonovo.FlatAppearance.BorderColor = System.Drawing.Color.DarkRed;
+            this.btnPonovo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPonovo.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnPonovo.ForeColor = System.Drawing.Color.White;
+            this.btnPonovo.Location = new System.Drawing.Point(16, 556);
+            this.btnPonovo.Name = "btnPonovo";
+            this.btnPonovo.Size = new System.Drawing.Size(277, 62);
+            this.btnPonovo.TabIndex = 32;
+            this.btnPonovo.Text = "PONOVO IGRAJ";
+            this.btnPonovo.UseVisualStyleBackColor = false;
+            this.btnPonovo.Click += new System.EventHandler(this.btnPonovo_Click);
+            // 
+            // btnZatvori
+            // 
+            this.btnZatvori.BackColor = System.Drawing.Color.DarkRed;
+            this.btnZatvori.FlatAppearance.BorderColor = System.Drawing.Color.DarkRed;
+            this.btnZatvori.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnZatvori.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnZatvori.ForeColor = System.Drawing.Color.White;
+            this.btnZatvori.Location = new System.Drawing.Point(365, 556);
+            this.btnZatvori.Name = "btnZatvori";
+            this.btnZatvori.Size = new System.Drawing.Size(277, 62);
+            this.btnZatvori.TabIndex = 33;
+            this.btnZatvori.Text = "ZATVORI";
+            this.btnZatvori.UseVisualStyleBackColor = false;
+            this.btnZatvori.Click += new System.EventHandler(this.btnZatvori_Click);
             // 
             // Rezultat
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(678, 582);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(678, 646);
+            this.Controls.Add(this.btnZatvori);
+            this.Controls.Add(this.btnPonovo);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -172,7 +273,6 @@
             this.Name = "Rezultat";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Lotto 7/35";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Rezultat_FormClosing);
             this.Load += new System.EventHandler(this.Rezultat_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -190,6 +290,13 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Button btnPonovo;
+        private System.Windows.Forms.Button btnZatvori;
     }
 }
--- Lotto/Rezultat.cs
+++ Lotto/Rezultat.cs
@@ -15,6 +15,9 @@ namespace Lotto_7_35
     public partial class Rezultat : Form
     {
         private List<int> listaOdabranihBrojeva;
+        private List<int> listaGeneriranihBrojeva = new List<int>();
+        private int brojacPogodenih;
+
         public Rezultat(List<int> ulaznaLista)
         {
             listaOdabranihBrojeva = ulaznaLista.OrderBy(x => x).ToList();
@@ -23,38 +26,139 @@ namespace Lotto_7_35
             //Linija ispod naslova
             lineLbl.BorderStyle = BorderStyle.Fixed3D;
             label5.BorderStyle = BorderStyle.Fixed3D;
-
-            OdabraniBrojevi();
-        }                                                                                                    
-                                                    
-        //Metoda koja je prosljeđena iz Lotto-a, korisnički odabrani brojevi
-        private void OdabraniBrojevi()
-        {
-            foreach (var item in listaOdabranihBrojeva)
-            {
-                label4.Text += item.ToString() + " ";
-            }
         }
 
         private void Rezultat_Load(object sender, EventArgs e)
         {
-            Environment.SetEnvironmentVariable("SWI_HOME_DIR", @"swipl");
-            Environment.SetEnvironmentVariable("Path", @"swipl\bin");
+            Environment.SetEnvironmentVariable("SWI_HOME_DIR", @"prolog");
+            Environment.SetEnvironmentVariable("Path", @"prolog");
+            Environment.SetEnvironmentVariable("Path", @"prolog\bin");
             string[] p = { "-q", "-f", @"lotto.pl" };
-            PlEngine.Initialize(p);
+
+            if (!PlEngine.IsInitialized)
+                PlEngine.Initialize(p);
+
+            PlQuery generatorBrojeva = new PlQuery("lotto(7,35,L)");
+            foreach (PlQueryVariables randomBrojevi in generatorBrojeva.SolutionVariables)
+            {
+                int brojacIndexa = 0;
+                foreach (var item in listaOdabranihBrojeva)
+                {
+                    PlQuery lotto = new PlQuery("findnum("+item.ToString()+","+randomBrojevi["L"].ToString()+",Ispis)");
+                    foreach (PlQueryVariables provjeraBrojeva in lotto.SolutionVariables)
+                    {
+                        if (provjeraBrojeva["Ispis"].ToString() == "Pronaden")
+                        {
+                                if (brojacIndexa == 0)
+                                    label7.ForeColor = Color.Green;
+                                else if (brojacIndexa == 1)
+                                    label9.ForeColor = Color.Green;
+                                else if (brojacIndexa == 2)
+                                    label11.ForeColor = Color.Green;
+                                else if (brojacIndexa == 3)
+                                    label10.ForeColor = Color.Green;
+                                else if (brojacIndexa == 4)
+                                    label14.ForeColor = Color.Green;
+                                else if (brojacIndexa == 5)
+                                    label13.ForeColor = Color.Green;
+                                else if (brojacIndexa == 6)
+                                    label12.ForeColor = Color.Green;
+                            brojacPogodenih++;
+                            break;
+                        }
+                        else if (provjeraBrojeva["Ispis"].ToString() == "Nije pronaden")
+                        {
+                            for (int i = 0; i < listaOdabranihBrojeva.Count; i++)
+                            {
+                                if (brojacIndexa == 0)
+                                    label7.ForeColor = Color.Black;
+                                else if (brojacIndexa == 1)
+                                    label9.ForeColor = Color.Black;
+                                else if (brojacIndexa == 2)
+                                    label11.ForeColor = Color.Black;
+                                else if (brojacIndexa == 3)
+                                    label10.ForeColor = Color.Black;
+                                else if (brojacIndexa == 4)
+                                    label14.ForeColor = Color.Black;
+                                else if (brojacIndexa == 5)
+                                    label13.ForeColor = Color.Black;
+                                else if (brojacIndexa == 6)
+                                    label12.ForeColor = Color.Black;
+                            }
+                            break;
+                        }
+                    }
+                    brojacIndexa++;
+                }
+
+                string splitBrojeva = randomBrojevi["L"].ToString();
+                splitBrojeva = splitBrojeva.Trim('[');
+                splitBrojeva = splitBrojeva.Trim(']');
+                string[] splitBrojevaLista = splitBrojeva.Split(',');
+
+                foreach (var broj in splitBrojevaLista)
+                {
+                    label4.Text += broj + " ";
+                }
+
+                label1.Text += brojacPogodenih;
+
+                label7.Text = listaOdabranihBrojeva[0].ToString();
+                label9.Text = listaOdabranihBrojeva[1].ToString();
+                label11.Text = listaOdabranihBrojeva[2].ToString();
+                label10.Text = listaOdabranihBrojeva[3].ToString();
+                label14.Text = listaOdabranihBrojeva[4].ToString();
+                label13.Text = listaOdabranihBrojeva[5].ToString();
+                label12.Text = listaOdabranihBrojeva[6].ToString();
+
+                if (brojacPogodenih == 0)
+                {
+                    label8.Text += "0 kn";
+                }
+                else if (brojacPogodenih == 1)
+                {
+                    label8.Text += "17,84 kn";
+                }
+                else if (brojacPogodenih == 2)
+                {
+                    label8.Text += "114,54 kn";
+                }
+                else if (brojacPogodenih == 3)
+                {
+                    label8.Text += "217,96 kn";
+                }
+                else if (brojacPogodenih == 4)
+                {
+                    label8.Text += "871,84 kn";
+                }
+                else if (brojacPogodenih == 5)
+                {
+                    label8.Text += "8.718,40 kn";
+                }
+                else if (brojacPogodenih == 6)
+                {
+                    label8.Text += "6.240.000 kn";
+                }
+                else if (brojacPogodenih == 7)
+                {
+                    label8.Text += "7.500.000 kn";
+                }
+
+            }
         }
 
-        private void Rezultat_FormClosing(object sender, FormClosingEventArgs e)
+        private void btnPonovo_Click(object sender, EventArgs e)
         {
-            PlQuery q = new PlQuery("zatvoriDatoteku");
-            q.NextSolution();
+            lotoForm lottoForm = new lotoForm();
+            this.Hide();
+            lottoForm.ShowDialog();
+            this.Close();
+        }
+
+        private void btnZatvori_Click(object sender, EventArgs e)
+        {
             PlEngine.PlCleanup();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            PlQuery lotto = new PlQuery("test()");
-            MessageBox.Show(lotto.ToString());
+            this.Close();
         }
     }
 }
